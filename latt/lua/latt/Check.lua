79d096b9b48610475e4df0088aa47667436ba667
Rodrigo Henrique <rhenrique@luaforge.net>
2007-03-13 18:36:10 +0000

    Completando as mensagens de erro.

--- latt/lua/latt/Check.lua
+++ latt/lua/latt/Check.lua
@@ -10,42 +10,42 @@ module("latt.Check", oop.class)
 function assertError(f, ...)
   local success = pcall(f, unpack(arg))
   if success then
-    error()
+    error("Function shouldn't run successfully.", 2)
   end
 end
 
 function assertEquals(expected, actual)
   if expected ~= actual then
-    error()
+    error("The expected value ["..tostring(expected).."] must be equal the actual value ["..tostring(actual).."].", 2)
   end
 end
 
 function assertNotEquals(expected, actual)
   if expected == actual then
-    error("The expected value ("..expected..") shouldn't be equal the actual value ("..actual..").")
+    error("The expected value ["..tostring(expected).."] shouldn't be equal the actual value ["..tostring(actual).."].", 2)
   end
 end
 
 function assertTrue(condition)
   if condition == false then
-    error("Condition should be true.", 2)
+    error("The condition must be true.", 2)
   end
 end
 
 function assertFalse(condition)
   if condition == true then
-    error("Condition should be false.", 2)
+    error("The condition must be false.", 2)
   end
 end
 
 function assertNil(variable)
   if variable ~= nil then
-    error("Variable should be nil: "..tostring(variable))
+    error("The variable must be nil. Actual value: ["..tostring(variable).."].", 2)
   end
 end
 
 function assertNotNil(variable)
   if variable == nil then
-    error()
+    error("The variable shouldn't be nil.", 2)
   end
 end
